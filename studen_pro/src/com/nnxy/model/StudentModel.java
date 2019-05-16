54366b9382a8e8302a610a34babb1a1760528942
wuliangning <1123205606@qq.com>
2019-05-16 09:06:25 +0800

    初始化工程

---
+++ studen_pro/src/com/nnxy/model/StudentModel.java
@@ -0,0 +1,44 @@
+package com.nnxy.model;
+
+public class StudentModel {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public StudentModel(int id, String username, String password, int sex) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+	}
+	@Override
+	public String toString() {
+		return "StudentModel [id=" + id + ", username=" + username + ", password=" + password + ", sex=" + sex + "]";
+	}
+	
+}
